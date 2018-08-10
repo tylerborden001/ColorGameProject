f7cc2e623e5129d7069ddca88124243274b9cc52
Tyler Borden <tylerborden373@gmail.com>
2018-08-10 15:55:53 -0500

    new game button is now functional.

--- ColorGameProject.js
+++ ColorGameProject.js
@@ -1,15 +1,25 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
+
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+  //generate all new colors
+  colors = generateRandomColors(6)
+  //pick a new random color from array
+  pickedColor = pickColor();
+   //change colorDisplay to match pickedColor
+   colorDisplay.textContent = pickedColor;
+  //change colors of squares
+  for(var i = 0;i < squares.length; i++){
+    squares[i].style.background = colors[i];
+  }
+})
+
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
@@ -23,6 +33,7 @@ for(var i = 0; i < squares.length; i++){
     //compare color to pickedColor
     if(clickedColor === pickedColor){
       messageDisplay.textContent = "Correct!";
+      h1.style.background = clickedColor;
       changeColors(clickedColor)
     } else {
       this.style.background = "#232323";
@@ -42,4 +53,28 @@ function changeColors(color){
 function pickColor(color){
 var random = Math.floor(Math.random() * colors.length)
 return colors[random];
+}
+
+function generateRandomColors(num){
+  //make an array
+  var arr = []
+  //add num random colors to array
+  for(var i = 0; i < num; i++){
+    //get random color and push into arr
+
+    arr.push(randomColor())
+
+  }
+  //return that array
+  return arr;
+}
+
+function randomColor(){
+  //pick a "red" from 0 -255
+  var r = Math.floor(Math.random() * 256);
+  //pick a green from 0 -255
+  var g = Math.floor(Math.random() * 256);
+  //pick a blue from 0  -255
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g +", " + b + ")";
 }
--- README.txt
+++ README.txt
@@ -9,3 +9,4 @@ August 10th 2018
   -Created ColorGameProject
   -Set up basic html and did some basic styling in css.Also made boxes responsive in js.
   -Boxes now change color according to which one was selected.
+  -"New Color" button is now functional.
